ad119a6f5a5757b2ff5e332af67b5b7ab16edc34
jkinberg <jkinberg@gmail.com>
2014-03-05 00:20:39 +0000

    Added post insert method. Sort posts_list by submitted date. Added accounts-ui config

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,5 +1,5 @@
 Template.postsList.helpers({
   posts: function() {
-    return Posts.find({}, {sort:{likeCount:-1, title:1}});
+    return Posts.find({}, {sort:{submitted:-1}});
   }
 });
--- collections/posts.js
+++ collections/posts.js
@@ -1,8 +1,32 @@
 Posts = new Meteor.Collection('posts');
 
-Posts.allow({
-  insert: function(userId, doc){
-    // return if userId exists (i.e. user is logged-in)
-    return !! userId;
+Meteor.methods({
+  post: function(postAttributes) {
+    var user = Meteor.user(), 
+        postWithSameLink = Posts.findOne({url:postAttributes.url});
+    
+    //ensure user is logged-in
+    if (!user)
+      throw new Meteor.Error(401, "You need to login to post new stories");
+    
+    //ensure post has a title
+    if (!postAttributes.title)
+      throw new Meteor.Error(422, "Please fill in a title");
+    
+    //check that there are not previous posts with same link
+    if (postAttributes.url && postWithSameLink) {
+      throw new Meteor.Error(302, "This link has already been posted", postWithSameLink._id);
+    }
+    
+    //pick out the whitelisted keys
+    var post = _.extend(_.pick(postAttributes, 'url', 'title', 'message'), {
+      userId: user._id,
+      author: user.username,
+      submitted: new Date().getTime()
+    });
+    
+    var postId = Posts.insert(post);
+    
+    return postId;
   }
 });
